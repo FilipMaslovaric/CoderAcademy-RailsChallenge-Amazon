39b686c50af5d6829d2298bff89f97b3e958b4c7
FilipMaslovaric <filip.maslovaric@gmail.com>
2017-10-11 13:35:19 +1000

    Created Store model, populated stores, added full_address helper

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171010175927) do
+ActiveRecord::Schema.define(version: 20171011031936) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,17 @@ ActiveRecord::Schema.define(version: 20171010175927) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  create_table "stores", force: :cascade do |t|
+    t.string "title"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "postcode"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
