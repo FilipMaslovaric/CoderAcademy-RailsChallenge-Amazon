211d4419770669816fb8bc51a9de8f80c825d264
FilipMaslovaric <filip.maslovaric@gmail.com>
2017-10-11 15:41:18 +1000

    Added Lat and Long coordinates to all store addresses via geocoder gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171011031936) do
+ActiveRecord::Schema.define(version: 20171011053216) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20171011031936) do
     t.string "country"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
 end
